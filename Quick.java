4d8cf809f7c9fe111527dccda9232af4aa487da1
Innertuber <bstringham@stuy.edu>
2019-03-12 03:05:29 -0400

    Fixed the two cases for infinite loops: repeats or at the end.

--- Quick.java
+++ Quick.java
@@ -4,20 +4,23 @@ public class Quick {
 		int end = values.length - 1;
 		while (true) {
 			int parted = partition(values, start, end);
+			//System.out.println(parted);
 			if (parted < k) {
 				start = parted;
-			}
-			if (parted > k) {
+			} else if (parted > k) {
 				end = parted;
-			}
-			if (parted == k) {
+			} else {
 				return values[k];
 			}
+			/*for (int i = 0; i < values.length; i++) {
+				System.out.print(values[i] + " ");
+			}
+			System.out.println(start + ", " + end);*/
 		}
 	}
 
 	private static int partition(int[] values, int low, int high) {
-		int randPos = (int)(Math.random() * (high - low) + low);
+		int randPos = (int)(Math.random() * (high - low + 1) + low);
 		//System.out.println(randPos);
 		int pivot = values[randPos];
 		//System.out.println(pivot);
@@ -33,16 +36,22 @@ public class Quick {
 				values[start] = values[end];
 				values[end] = current;
 				end--;
+				//System.out.println(end);
 			}
 		}
 		if (values[end] > pivot) {
 			values[low] = values[end - 1];
 			values[end - 1] = pivot;
+			//System.out.println(end - 1);
 			return end - 1;
-		} else {
+		} else if (values[end] < pivot) {
 			values[low] = values[end];
 			values[end] = pivot;
+			//System.out.println(end);
 			return end;
+		} else {
+			//System.out.println((int)(Math.round(Math.random())));
+			return (int)(end - Math.round(Math.random()));
 		}
 	}
 }
