3f78d70d508c7c087b14a4ac7685d688751a0c21
Innertuber <bstringham@stuy.edu>
2019-03-12 01:18:48 -0400

    Added in a poorly tested partition and a driver skeleton

--- Quick.java
+++ Quick.java
@@ -1,5 +1,35 @@
 public class Quick {
-	public static int quickSelect(int[] values, int k) {
+	public static int quickselect(int[] values, int k) {
 		return 52396;
+	}
+
+	private int partition(int[] values, int n) {
+		int randPos = (int)(Math.random() * values.length);
+		int pivot = values[randPos];
+		int start = 1;
+		int end = values.length - 1;
+		values[randPos] = values[0];
+		values[0] = pivot;
+		for (int i = 1; i < end; i++) {
+			current = values[i];
+			if (values[i] <= pivot) {
+				values[i] = values[start];
+				values[start] = current;
+				start++;
+			} else if (values[i] > pivot) {
+				values[i] = values[end];
+				values[end] = current;
+				end--;
+			}
+		}
+		if (values[end] > pivot) {
+			values[0] = values[end - 1];
+			values[end - 1] = pivot;
+			return end - 1;
+		} else {
+			values[0] = values[end];
+			values[end] = pivot;
+			return end;
+		}
 	}
 }
